000d62855c59448aee4abae78e6d5b3b000782fc
Sergey Malyuga <sergey@malyuga.ru>
2023-08-16 13:26:21 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/controller/exception/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.controller.exception;
-
-
-import java.io.IOException;
-public class ValidationException extends RuntimeException {
-    public ValidationException(String string){
-        super(string);
-    }
-}
