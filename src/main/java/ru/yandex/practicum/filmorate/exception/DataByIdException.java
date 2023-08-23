28c3f75551bd8bd02f9ac5ee58d49a284d896a16
Sergey Malyuga <sergey@malyuga.ru>
2023-08-23 13:47:33 +0300

    style: DataByIdExcepyion whitespace

--- src/main/java/ru/yandex/practicum/filmorate/exception/DataByIdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/DataByIdException.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class DataByIdException extends RuntimeException {
+
     public DataByIdException(String message){
         super(message);
     }
