967cd3827a5baa446222a1cc5606d3646b91614a
Sergey Malyuga <sergey@malyuga.ru>
2023-08-23 13:55:53 +0300

    style: separate method from class name

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
     Film changeFilm(Film film);
     Film getFilmById(int id);
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User addUser(User user);
     User addChangeUser(User user);
     User getUserById(int id);
